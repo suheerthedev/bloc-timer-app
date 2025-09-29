eda6623f3c7dae2e874173db2a9f5faf6c533011
suheerthedev <khansuheer1@gmail.com>
2025-09-29 20:10:21 +0500

    Handled all the events in TimerBloc

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_timer_app/ticker.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -6,9 +9,59 @@ part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  TimerBloc() : super(TimerInitial()) {
-    on<TimerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final Ticker _ticker;
+  static const int _duration = 60;
+
+  StreamSubscription<int>? _tickerSubcription;
+
+  TimerBloc({required Ticker ticker})
+    : _ticker = ticker,
+      super(TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
+    on<TimerReset>(_onReset);
+    on<_TimerTicked>(_onTicked);
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubcription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubcription?.cancel();
+    _tickerSubcription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(_TimerTicked(duration: duration)));
+  }
+
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubcription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onResumed(TimerResumed event, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubcription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
+  }
+
+  void _onReset(TimerReset event, Emitter<TimerState> emit) {
+    _tickerSubcription?.cancel();
+    emit(const TimerInitial(_duration));
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(
+      event.duration > 0
+          ? TimerRunInProgress(event.duration)
+          : TimerRunComplete(),
+    );
   }
 }
