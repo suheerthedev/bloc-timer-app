939b09ce3b72bce0335af9a93c66b3aa93444004
suheerthedev <khansuheer1@gmail.com>
2025-09-29 19:51:02 +0500

    Implemented TimerEvents

--- lib/timer/bloc/timer_event.dart
+++ lib/timer/bloc/timer_event.dart
@@ -1,4 +1,28 @@
 part of 'timer_bloc.dart';
 
 @immutable
-sealed class TimerEvent {}
+sealed class TimerEvent {
+  const TimerEvent();
+}
+
+final class TimerStarted extends TimerEvent {
+  final int duration;
+  const TimerStarted({required this.duration});
+}
+
+final class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+final class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+final class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class _TimerTicked extends TimerEvent {
+  final int duration;
+  const _TimerTicked({required this.duration});
+}
