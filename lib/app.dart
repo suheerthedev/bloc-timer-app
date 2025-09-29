7bb3ed67fbee2ab5a0439c9050b68324e1b360be
suheerthedev <khansuheer1@gmail.com>
2025-09-29 20:20:05 +0500

    Updated timer.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_timer_app/timer/timer.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
---
+++ lib/timer/timer.dart
@@ -0,0 +1,67 @@
+import 'package:bloc_timer_app/ticker.dart';
+import 'package:bloc_timer_app/timer/bloc/timer_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Timer',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.teal,
+      ),
+      body: Stack(
+        children: [
+          // const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100),
+                child: Center(child: TimerText()),
+              ),
+              // Actions()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr = ((duration / 60) % 60).floor().toString().padLeft(
+      2,
+      '0',
+    );
+    final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondsStr',
+      style: Theme.of(context).textTheme.headlineLarge,
+    );
+  }
+}
