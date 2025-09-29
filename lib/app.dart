6ef125cb597ac66acfbef322c1b3fe0dd2cffe25
suheerthedev <khansuheer1@gmail.com>
2025-09-29 20:21:18 +0500

    Updated timer.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_timer_app/timer/timer.dart';
+import 'package:bloc_timer_app/timer/view/timer_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
